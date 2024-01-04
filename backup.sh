b0193402b3a31abd90ab34c7762ac6572d0017fe
Craig Hays <craig@craighays.com>
2024-01-04 15:22:45 +0000

    Update backup.sh
    added ntfy.sh notification

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 NOW=$(date '+%Y-%m-%d-%H-%M-%S')
 BACKUP="/tmp/backup-$NOW"
 BACKUPZIP="/tmp/backup-$NOW.zip"
+S3BUCKET="<REPLACE_WITH_YOUR_S3_BUCKET_NAME>"
 echo $BACKUP
 mkdir $BACKUP
 cp -R /etc/apache2 $BACKUP/
@@ -11,6 +12,7 @@ cp -R /var/www $BACKUP/
 mkdir $BACKUP/databases
 mysqldump --all-databases > $BACKUP/databases/all-databases.sql
 zip -r $BACKUPZIP $BACKUP
-aws s3 cp $BACKUPZIP s3://<REPLACE_WITH_YOUR_S3_BUCKET_NAME/
+aws s3 cp $BACKUPZIP s3://$S3BUCKET/
+curl -d "Backup complete and $BACKUPZIP uploaded" ntfy.sh/$S3BUCKET
 rm -rf $BACKUP
 rm -rf $BACKUPZIP
